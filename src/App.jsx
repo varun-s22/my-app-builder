ec3a2117ab4fea48c2badbc9fb351eaaec3f05f4
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2023-08-18 16:25:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+// You can split your components
+import Editor from "./Editor";
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* Feel free to delete the header */}
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. (Delete me if you
+          like!)
+        </p>
+      </header>
+      <Editor />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Editor.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Editor.css";
+
+const EditorCanvas = (props) => {
+  return (
+    <div className="editor-canvas">
+      <h4> Put the drag and drop interface over here! </h4>
+    </div>
+  );
+};
+
+const EditorPicker = (props) => {
+  return (
+    <div className="editor-picker">
+      <h4>
+        {" "}
+        Clicking these buttons should create new components on the canvas{" "}
+      </h4>
+      <button> Create a button </button>
+      <button> Create a text input </button>
+      <button> Create a dropdown </button>
+      <button> Create a table </button>
+    </div>
+  );
+};
+
+const Editor = (props) => {
+  return (
+    <div className="editor">
+      <EditorCanvas />
+      <EditorPicker />
+    </div>
+  );
+};
+
+export default Editor;
